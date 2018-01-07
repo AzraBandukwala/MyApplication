e3cf8f1e9ef6cb691b5cc1c2eaa8b9615ab9be6e
Azra Bandukwala <azrabandukwala@Azras-MacBook-Pro.local>
2018-01-07 00:57:03 -0800

    Initial commit

---
+++ app/src/main/java/com/example/azrabandukwala/myapplication/Home.java
@@ -0,0 +1,39 @@
+package com.example.azrabandukwala.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class Home extends AppCompatActivity {
+
+   // private TextView mTextMessage;
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //mTextMessage = (TextView) findViewById(R.id.message);
+
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
